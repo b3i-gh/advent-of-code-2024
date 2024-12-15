477009bccad37a53a84268223e1fb7c7e230d82b
b3i <b3i.gh@protonmail.com>
2024-12-15 12:13:36 +0100

    day 13: linear algebra. solution found online

---
+++ src/day13.py
@@ -0,0 +1,32 @@
+import re
+
+def part1(puzzle_input):
+    total = 0
+    tolerance = 0.0001
+    for machine in puzzle_input.split('\n\n'):
+        ax, ay, bx, by, x, y = map(int, re.findall(r'(\d+)', machine))
+        A = (bx*y - by*x) / (bx*ay - by*ax)
+        B = (x-ax*A) / bx
+        if abs(A - round(A)) < tolerance and abs(B - round(B)) < tolerance:
+            total += 3*A + B
+
+    return int(total)
+
+def part2(puzzle_input):
+    total = 0
+    tolerance = 0.0001
+    offset = 10_000_000_000_000
+    for machine in puzzle_input.split('\n\n'):
+        ax, ay, bx, by, x, y = map(int, re.findall(r'(\d+)', machine))
+        x += offset
+        y += offset
+        A = (bx*y - by*x) / (bx*ay - by*ax)
+        B = (x-ax*A) / bx
+        if abs(A - round(A)) < tolerance and abs(B - round(B)) < tolerance:
+            total += 3*A + B
+
+    return int(total)
+
+
+def main(input):
+    return part1(input), part2(input)
